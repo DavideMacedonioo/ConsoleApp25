449d8ab97ae8ff52562040b99ca759ceaffd6224
francesco <francesco@CICO>
2024-02-20 18:03:52 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(4,7);
+            Vector b = new Vector(5,6);
+            Vector somma = a + b;
+            Console.WriteLine("vettore somma: a + b di componenti {0} {1}",somma.x, somma.y);
+            //Vector differenza = a - b;
+            //Console.WriteLine("vettore differenza: a - b di componenti {0} {1}", differenza.x, differenza.y);
+            Console.WriteLine("vettore differenza: a - b ({0}) ",(a - b).ToString());
+            Console.WriteLine("vettore +a : ({0})",(+a).ToString());
+            Console.WriteLine("vettore -b : ({0})",(-b).ToString());
+            Console.WriteLine("il prodotto scalare tra il vettore a e il vettore b è : {0}", (a * b).ToString());
+            double z=4;
+            Console.WriteLine("prodotto tra vettore a e scalare z : ({0})", (a * z).ToString());
+            Console.WriteLine("prodotto tra scalare a e vettore z : ({0})", (z * b).ToString());
+            Console.WriteLine("divisione tra vettore a e scalare z : ({0})", (a / z).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp25/Vector.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Vector
+    {
+        public double x { get; }
+        public double y { get; }
+        public Vector (double x, double y)
+        {
+            this.x = x; 
+            this.y = y;
+        }
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.x + b.x, a.y + b.y);
+        }
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.x-b.x, a.y-b.y);
+        }
+        public static Vector operator +(Vector v)
+        {
+            return v;
+        }
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.x, -v.y);
+        }
+        public string ToString()
+        {
+            return "" + x + ";" + y;
+        }
+        public static double operator *(Vector a, Vector b)
+        {
+            double scalare = Math.Sqrt(Math.Pow(a.x - b.x, 2) + Math.Pow(a.y - b.y, 2));
+            return scalare;
+        }
+        public static Vector operator *(Vector a, double z)
+        {
+            return new Vector(a.x * z, a.y * z);
+        }
+        public static Vector operator *(double z, Vector b)
+        {
+            return new Vector(z * b.x, z * b.y);
+        }
+        public static Vector operator /(Vector a, double z)
+        {
+            return new Vector(a.x/z, a.y/z);
+        }
+    }
+}
